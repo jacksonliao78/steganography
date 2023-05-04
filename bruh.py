fc309babf45453e42e3946b0161d0c7d238c6d76
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2023-05-03 20:48:12 -0400

    finished encoding and save alg

--- bruh.py
+++ bruh.py
@@ -1,7 +1,7 @@
 
 from PIL import Image
 
-def toBinary(msg):
+def to_binary(msg):
     binary = [(format(ord(word), '08b')) for word in msg]
     return [*''.join(binary)]
 
@@ -11,29 +11,31 @@ def encode():
     msg = input("Type in a message to encode: ") 
     msg += "@@@@@"
 
-    newImage = image.copy().convert('RGB')
+    new_image = image.copy().convert('RGB')
 
-    r, g, b = newImage.getpixel((1, 1))
+    r, g, b = new_image.getpixel((1, 1))
 
-    newImage.putdata(encodePix(newImage, toBinary(msg)))
+    new_image.putdata(encode_pix(new_image, to_binary(msg)))
+    new_image_name = input("Enter the new image name with extension: ")
+    new_image.save(new_image_name)
     
-def encodePix(img, msg):
+def encode_pix(img, msg):
     
-    imageData = list(img.getdata())
-    modifiedData = []
+    image_data = list(img.getdata())
+    modified_data = []
 
     for i in range(len(msg) // 3 + 2):
-        temp = list(imageData[i])
+        temp = list(image_data[i])
         for j in range(3):
-            val = list(bin(imageData[i][j])[2:].zfill(8)) 
+            val = list(bin(image_data[i][j])[2:].zfill(8)) 
             if i*3 + j < len(msg) and val[-1] == '1' and msg[i*3 + j] == '0':
                 val[-1] = '0'
             elif i*3 + j < len(msg) and val[-1] == '0' and msg[i*3 + j] == '1':
                 val[-1] = '1'
             temp[j] = int(''.join(val), 2) 
-        modifiedData.append(tuple(temp))
-    modifiedData += imageData[len(modifiedData):]        
-    return modifiedData
+        modified_data.append(tuple(temp))
+    modified_data += image_data[len(modified_data):]        
+    return modified_data
 
 encode()
     
