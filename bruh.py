b8c5aab00a622b931d6da798c132d123bad7eaa1
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2023-05-06 19:13:08 -0400

    finished bulk of steganography program

--- bruh.py
+++ bruh.py
@@ -9,6 +9,7 @@ def encode():
     image = Image.open(input("Enter your image with extension: "), 'r')
     
     msg = input("Type in a message to encode: ") 
+    #to detect the end of a message
     msg += "@@@@@"
 
     new_image = image.copy().convert('RGB')
@@ -25,17 +26,44 @@ def encode_pix(img, msg):
     modified_data = []
 
     for i in range(len(msg) // 3 + 2):
+
+        #creates a list of the rgb values for a pixel
         temp = list(image_data[i])
         for j in range(3):
+
+            #r, g, or b val as a binary number to be modified
             val = list(bin(image_data[i][j])[2:].zfill(8)) 
             if i*3 + j < len(msg) and val[-1] == '1' and msg[i*3 + j] == '0':
                 val[-1] = '0'
             elif i*3 + j < len(msg) and val[-1] == '0' and msg[i*3 + j] == '1':
                 val[-1] = '1'
+            
+            #reassigning the rgb val, then appending the new pixel to modified_data
             temp[j] = int(''.join(val), 2) 
         modified_data.append(tuple(temp))
+    
+    #adding the unmodified data
     modified_data += image_data[len(modified_data):]        
     return modified_data
 
-encode()
+def decode():
+    msg = ''
+    image = Image.open(input("Enter your image with extension: "))
+    print(list(image.getdata())[:10])
+
+    image_data = ""
+    for pixel in list(image.getdata())[:10]:
+        r, g, b = tuple(pixel)
+        r, g, b = bin(r)[2:].zfill(8), bin(g)[2:].zfill(8), bin(b)[2:].zfill(8)
+        image_data += '0' if r[-1] == '0' else '1'
+        image_data += '0' if g[-1] == '0' else '1'
+        image_data += '0' if b[-1] == '0' else '1'
+
+    print(image_data)
+
+    while True:
+        pass
+
+#encode()
+decode()
     
